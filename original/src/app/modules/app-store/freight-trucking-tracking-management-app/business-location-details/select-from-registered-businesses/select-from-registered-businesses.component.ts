8243d318873a7e1197fb8771fab1d2154e1a5fe5
ㅤ <107886961+ErionNezha@users.noreply.github.com>
2026-09-24 00:18:34 +0200

    Publikimi fillestar — demo live + kodi burim

---
+++ original/src/app/modules/app-store/freight-trucking-tracking-management-app/business-location-details/select-from-registered-businesses/select-from-registered-businesses.component.ts
@@ -0,0 +1,168 @@
+import { HttpClient } from '@angular/common/http';
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
+import { MatPaginator } from '@angular/material/paginator';
+import { MatTableDataSource } from '@angular/material/table';
+import * as XLSX from 'xlsx';
+import { SelectionModel } from '@angular/cdk/collections';
+
+
+@Component({
+  selector: 'app-select-from-registered-businesses',
+  templateUrl: './select-from-registered-businesses.component.html',
+  styleUrls: ['./select-from-registered-businesses.component.scss']
+})
+export class SelectFromRegisteredBusinessesComponent implements OnInit {
+
+  constructor(
+    private _httpClient: HttpClient,
+    ) {
+    }
+
+    ngOnInit() {
+    }
+
+  
+
+    // table code 
+
+  filterValue = '';
+  applyFilter(event: Event) {
+    this.filterValue = (event.target as HTMLInputElement).value;
+    this.filterValue = this.filterValue.trim(); // Remove whitespace
+    this.filterValue = this.filterValue.toLowerCase(); // Datasource defaults to lowercase matches
+    this.dataSource.filter = this.filterValue;
+  }
+
+  displayedColumns: string[] = ['select','business_name', 'contact_address','block_status'];
+
+
+
+  dataSource = new MatTableDataSource<PeriodicElement>(ELEMENT_DATA);
+  selection = new SelectionModel<PeriodicElement>(true, []);
+
+  @ViewChild(MatPaginator) paginator!: MatPaginator;
+
+  @ViewChild('paginatorElement', { read: ElementRef })
+  paginatorHtmlElement!: ElementRef;
+
+
+  rowValue: any[] = [];
+  /** Whether the number of selected elements matches the total number of rows. */
+  isAllSelected() {
+    const numSelected = this.selection.selected.length;
+    // console.log(this.selection.selected);
+    this.rowValue = this.selection.selected;
+    const numRows = this.dataSource.data.length;
+    return numSelected === numRows;
+  }
+
+  /** Selects all rows if they are not all selected; otherwise clear selection. */
+  masterToggle() {
+    this.isAllSelected()
+      ? this.selection.clear()
+      : this.dataSource.data.forEach((row) => this.selection.select(row));
+  }
+
+  pageSizeOptions: { [key: string]: string } = {
+    5: '5',
+    10: '10',
+    25: '25',
+    100: 'All',
+  };
+  showPageSizeOptions: boolean = true;
+
+
+
+
+  ngAfterViewInit() {
+    this.dataSource.paginator = this.paginator;
+    this.translateMatPaginator(this.paginator);
+  }
+
+  translateMatPaginator(paginator: MatPaginator) {
+    paginator._intl.firstPageLabel = 'First';
+    paginator._intl.itemsPerPageLabel = 'Records Per Page';
+    paginator._intl.lastPageLabel = 'Last';
+    paginator._intl.nextPageLabel = 'Next';
+    paginator._intl.previousPageLabel = 'Previous';
+  }
+
+  exportReport(fileName: any): void {
+    /* pass here the table id */
+    let element = document.getElementById('excel_table');
+    const ws: XLSX.WorkSheet = XLSX.utils.table_to_sheet(element);
+
+    /* generate workbook and add the worksheet */
+    const wb: XLSX.WorkBook = XLSX.utils.book_new();
+    XLSX.utils.book_append_sheet(wb, ws, 'Sheet1');
+
+    /* save to file */
+    XLSX.writeFile(wb, fileName);
+  }
+
+  
+
+  onPrint() {
+    window.print();
+  }
+
+  @ViewChild('pdfTable', { static: false }) pdfTable!: ElementRef;
+  public downloadAsPDF() {
+    const htmlToPrint =
+      '' +
+      '<style type="text/css">' +
+      '.pageFooter {' +
+      '    display: table-footer-group;' +
+      '    counter-increment: page;' +
+      '}' +
+      '.pageFooter:after {' +
+      '   content: "Page " counter(page)' +
+      '}' +
+      '</style>';
+    var printContents = document.getElementById('pdfTable')!.innerHTML;
+    let popupWin: any = window.open(
+      'Angular Large Table to pdf',
+      '_blank',
+      'width=768,height=auto'
+    );
+
+    popupWin.document.write(
+      '<html><head>' +
+        '<link rel="stylesheet" href="' +
+        'https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/css/bootstrap.min.css"/>' +
+        '<style type="text/css">' +
+        '.pageFooter {' +
+        '    display: table-footer-group;' +
+        '    counter-increment: page;' +
+        '}' +
+        '.pageFooter:after {' +
+        '   content: "Page " counter(page)' +
+        '}' +
+        '</style>' +
+        '</head><body onload="window.print();window.close()">' +
+        '<h1 style="text-align: center;font-size:30px">Welcome to GETster.TECH</h1>' +
+        '<h6 style="text-align: center;">Audit Trail</h6>' +
+        printContents +
+        '</body></html>'
+    );
+    popupWin.document.close();
+  }
+}
+
+
+export interface PeriodicElement {
+  business_name:any;
+  contact_address:any;
+  block_status: any;
+}
+
+
+const ELEMENT_DATA: PeriodicElement[] = [
+                         { business_name: 'Business Name 1',contact_address:'#31, MG Street,Hosur Krishnagiri,TamilNadu,India-635 109',block_status:"NotBlocked"},
+                         { business_name: '',contact_address:'',block_status:""},
+                         { business_name: '',contact_address:'',block_status:""},
+                         { business_name: '',contact_address:'',block_status:""},
+                         { business_name: '',contact_address:'',block_status:""},
+
+];
+
