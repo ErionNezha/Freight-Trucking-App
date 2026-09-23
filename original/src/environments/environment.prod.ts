798357a987552e46f46505d375c946313c50dc1f
ㅤ <107886961+ErionNezha@users.noreply.github.com>
2026-09-24 00:22:17 +0200

    Publikimi fillestar — demo live + kodi burim

---
+++ original/src/environments/environment.prod.ts
@@ -0,0 +1,14 @@
+export const environment = {
+  production: true,
+  baseURL_Live: 'https://apitemplate.getbiz.app',
+  // baseURL:'localhost:3000',
+
+
+  
+  form: 'https://apitemplate.getbiz.app/api/template-getbiz/form-controller/simple-form',
+  address_form: 'https://apitemplate.getbiz.app/api/template-getbiz/form-controller/address-form',
+  stepper_form: 'https://apitemplate.getbiz.app/api/template-getbiz/form-controller/stepper-form',
+  audit_trail: 'https://apitemplate.getbiz.app/api/template-getbiz/form-controller/audit-trail',
+  
+};
+
